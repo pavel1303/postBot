7d019d8729aebcf6614cb33d4499ae6a602f750a
Lebedev Pavel <lebedpavel.dev@gmail.com>
2025-02-16 16:51:35 +0300

    feat: modified promts and metod for post report

--- src/database/getMealsInfo.ts
+++ src/database/getMealsInfo.ts
@@ -1,9 +1,9 @@
 import { firestore } from ".";
-export const getMeals = async () => {
+export const getMealsInfo = async () => {
   const today = new Date().toISOString().split("T")[0];
   const reportDocRef = firestore.collection("reports").doc(today);
 
   const data = (await reportDocRef.get())?.data();
 
-  return data && data.meals ? data.meals : [];
+  return data || null;
 };
--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,19 @@ import { getPhotosUrlsFromUpdate } from "./helpers/getPhotosFromPost";
 import { geminiFlashRequest } from "./gpt-api/geminiImageRequest";
 import { createPromtForReportMeal } from "./prompt-generate/createPromptForReportMeal";
 import { createMeal } from "./database/createMeal";
-import { getMealsDescription } from "./helpers/getMealsDescription";
+import { getMealsMedia } from "./helpers/getMealsMedia";
 
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN as string);
+
+bot.use((ctx, next) => {
+  if (ctx?.from?.id === Number(process.env.ALLOWED_USER_ID)) {
+    next();
+  } else {
+    ctx.reply("Вы кто такие? Я вас не звал! Уходите!!!");
+  }
+});
 
 bot.use(new MediaGroup({ timeout: 1000 }).middleware());
 
@@ -30,21 +38,30 @@ bot.on(photo_media_group(), async (ctx: PhotoMediaGroupContext<Context>) => {
       description: modelResponse,
       date: new Date().toISOString().split("T")[0],
     });
-    await ctx.reply(
-      `Все отлично, получилась такоя вот красота:\n${modelResponse}`
-    );
+    await ctx.reply(`Все отлично, отчет создан и записан в БД`);
   } else {
     await ctx.reply(`Пу пу пууууу.... Что то не зашло :(`);
   }
 });
 
 bot.command("create_post", async (ctx: Context) => {
-  const mealsDescription = await getMealsDescription();
-
-  if (mealsDescription) {
-    await ctx.reply(`Вот твой отчет за день:\n\n${mealsDescription}`);
-  } else {
-    await ctx.reply(`Пу пу пууууу.... Что то не зашло :(`);
+  const { description, photos } = await getMealsMedia();
+  try {
+    if (description && photos.length) {
+      await bot.telegram.sendMessage(
+        process.env.CHANNEL_ID as string,
+        description
+      );
+      await bot.telegram.sendMediaGroup(
+        process.env.CHANNEL_ID as string,
+        photos
+      );
+      ctx.reply(`Отчет успешно опубликован`);
+    } else {
+      throw Error("Нет описания или изображений");
+    }
+  } catch (e) {
+    await ctx.reply(`Хъюстон, у нас проблема: ${(e as Error)?.message || ""}`);
   }
 });
 
